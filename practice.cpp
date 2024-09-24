23fdc66a18a2d74d2e0fb8a57e38a28660f88b40
amongsusman <149024306+amongsusman@users.noreply.github.com>
2024-09-24 08:31:59 -0400

    Add files via upload

---
+++ practice.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int age1, age2;
+    cout << "What is your age?" << "\n";
+    cin >> age1;
+    cout << "What is your friend's age?" << "\n";
+    cin >> age2;
+    if (age1 > age2) {
+        cout << "You are older than your friend." << "\n";
+    }
+    else if (age1 < age2) {
+        cout << "You are younger than your friend." << "\n";
+    }
+    else {
+        cout << "You guys have the same age." << "\n";
+    }
+    return 0;
+}
